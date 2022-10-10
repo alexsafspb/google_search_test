17c3de8770b8bd95af14cc38aa76e0cf20dc24cc
alexsafspb <alexsaf@yandex.ru>
2022-10-11 00:16:40 +0300

    removed normal run from test.py

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import search_google
 from selenium import webdriver
-import time
+
 
 SEARCH_TEXT = "Think24.ru"
 BAD_SEARCH_TEXT = "e;kghrke;jvb;kjvb/kvb'vbqe'vbqekjvbq;kfjbqekvjc"
 SEARCH_TITLE = "Think24.ru - Поиск в Google"
 LINK_NAME = "Think24 App"
 TARGET_URL = "https://think24.ru/"
+
+if __name__ == "__main__":
+    print("not implemented, please call with pytest \n \"python3 -m pytest -v  test.py\"")
+    exit(0)
 
 driver = webdriver.Chrome()
 Page = search_google.GoogleSearch(driver)
@@ -20,7 +24,7 @@ def test2_search_in_google():
     Page.search_word(SEARCH_TEXT)
     assert Page.result_stats
 
-def test3_find_in_search_result():
+def test3_find_link_in_search_result():
     Page.search_result_by_name(LINK_NAME)
     assert Page.check_title(SEARCH_TITLE)
 
@@ -33,11 +37,4 @@ def test4_click_on_search_result():
 def test_teardown():
     driver.close()
 
-if __name__ == "__main__":
 
-    test1_open_google()
-    test2_search_in_google()
-    test3_find_in_search_result()
-    test4_click_on_search_result()
-    time.sleep(1)
-    test_teardown()
